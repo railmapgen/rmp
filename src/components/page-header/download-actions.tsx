2acfe9dc83094168179c16e6e0d30e832161cc91
thekingofcity <3353040+thekingofcity@users.noreply.github.com>
2022-08-02 20:23:40 +0800

    #15 Download image

--- src/components/page-header/download-actions.tsx
+++ src/components/page-header/download-actions.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { IconButton, Menu, MenuButton, MenuList, MenuItem } from '@chakra-ui/react';
 import { MdDownload } from 'react-icons/md';
 import { useTranslation } from 'react-i18next';
+import { calculateCanvasSize } from '../../util/helpers';
 
 export default function DownloadActions() {
     const { t } = useTranslation();
@@ -9,7 +10,43 @@ export default function DownloadActions() {
     const graph = React.useRef(window.graph);
 
     const handleDownloadJson = () => {
-        downloadAs(`RMG_${new Date().valueOf()}.json`, 'application/json', JSON.stringify(graph.current.export()));
+        downloadAs(`RMP_${new Date().valueOf()}.json`, 'application/json', JSON.stringify(graph.current.export()));
+    };
+    // thanks to this article that includes every steps in converting svg to png
+    // https://levelup.gitconnected.com/draw-an-svg-to-canvas-and-download-it-as-image-in-javascript-f7f7713cf81f
+    const handleDownload = () => {
+        // get the minimum and maximum of the graph
+        const { xMin, yMin, xMax, yMax } = calculateCanvasSize(graph.current);
+        const [width, height] = [xMax - xMin, yMax - yMin];
+
+        const elem = document.getElementById('canvas')!.cloneNode(true) as SVGSVGElement;
+        // remove virtual nodes
+        [...elem.children].filter(e => e.id.startsWith('virtual_misc_node_virtual')).forEach(e => elem.removeChild(e));
+        // transform svg to contain all the nodes in the graph
+        // otherwise the later drawImage won't be able to display them all
+        elem.setAttribute('viewBox', `${xMin} ${yMin} ${width} ${height}`);
+        // append to document to render the svg
+        document.body.appendChild(elem);
+        // convert it to blob and url for drawing to canvas
+        const blob = new Blob([elem.outerHTML], { type: 'image/svg+xml;charset=utf-8' });
+        const blobURL = window.URL.createObjectURL(blob);
+        // release after use
+        document.body.removeChild(elem);
+
+        // create canvas to be drawn on
+        const canvas = document.createElement('canvas');
+        canvas.width = width;
+        canvas.height = height;
+        const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
+
+        const img = new Image();
+        img.onload = () => {
+            ctx.drawImage(img, 0, 0, width, height);
+            canvas.toBlob(blob => downloadBlobAs(`RMP_${new Date().valueOf()}.png`, blob!), 'image/png');
+        };
+        img.src = blobURL;
+
+        window.URL.revokeObjectURL(blobURL);
     };
 
     return (
@@ -17,6 +54,7 @@ export default function DownloadActions() {
             <MenuButton as={IconButton} size="sm" variant="ghost" icon={<MdDownload />} />
             <MenuList>
                 <MenuItem onClick={handleDownloadJson}>{t('header.download.config')}</MenuItem>
+                <MenuItem onClick={handleDownload}>{t('header.download.png')}</MenuItem>
             </MenuList>
         </Menu>
     );
--- src/util/helpers.ts
+++ src/util/helpers.ts
@@ -1,3 +1,6 @@
+import { MultiDirectedGraph } from 'graphology';
+import { NodeAttributes, EdgeAttributes, GraphAttributes } from '../constants/constants';
+
 export const getMousePosition = (e: React.MouseEvent) => {
     const bbox = e.currentTarget.getBoundingClientRect();
     const x = e.clientX - bbox.left;
@@ -5,3 +8,22 @@ export const getMousePosition = (e: React.MouseEvent) => {
     // console.log(e.clientX, bbox.left, e.clientY, bbox.top, x, y);
     return { x, y };
 };
+
+export const calculateCanvasSize = (graph: MultiDirectedGraph<NodeAttributes, EdgeAttributes, GraphAttributes>) => {
+    let [xMin, yMin, xMax, yMax] = [Number.MAX_VALUE, Number.MAX_VALUE, Number.MIN_VALUE, Number.MIN_VALUE];
+
+    graph.forEachNode((node, attr) => {
+        xMin = Math.min(attr.x, xMin);
+        yMin = Math.min(attr.y, yMin);
+        xMax = Math.max(attr.x, xMax);
+        yMax = Math.max(attr.y, yMax);
+    });
+
+    xMin -= 150;
+    yMin -= 150;
+    xMax += 150;
+    yMax += 150;
+
+    return { xMin, yMin, xMax, yMax };
+};
+
