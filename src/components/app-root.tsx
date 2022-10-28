4a94ab3bccde2ee23d4e79d1cd242930430ce58f
thekingofcity <3353040+thekingofcity@users.noreply.github.com>
2022-10-28 20:45:07 +0800

    #95 Error Boundary

--- src/components/app-root.tsx
+++ src/components/app-root.tsx
@@ -3,20 +3,22 @@ import WindowHeader from './page-header/window-header';
 import SvgWrapper from './svg-wrapper';
 import ToolsPanel from './panel/tools/tools';
 import DetailsPanel from './panel/details/details';
-import { RmgWindow } from '@railmapgen/rmg-components';
+import { RmgErrorBoundary, RmgWindow } from '@railmapgen/rmg-components';
 import { Flex } from '@chakra-ui/react';
 
 export default function AppRoot() {
     return (
         <RmgWindow>
             <WindowHeader />
-            <Flex direction="row" height="100%" overflow="hidden" sx={{ position: 'relative' }}>
-                {/* `position: 'relative'` is used to make sure RmgSidePanel in DetailsPanel
-                    have the right parent container for its `position: 'absolute'` calculation. */}
-                <ToolsPanel />
-                <SvgWrapper />
-                <DetailsPanel />
-            </Flex>
+            <RmgErrorBoundary allowReset>
+                <Flex direction="row" height="100%" overflow="hidden" sx={{ position: 'relative' }}>
+                    {/* `position: 'relative'` is used to make sure RmgSidePanel in DetailsPanel
+                        have the right parent container for its `position: 'absolute'` calculation. */}
+                    <ToolsPanel />
+                    <SvgWrapper />
+                    <DetailsPanel />
+                </Flex>
+            </RmgErrorBoundary>
         </RmgWindow>
     );
 }
