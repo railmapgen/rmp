afd6cb18ae42e5d0dfccfe53a5c8f75831c03c0e
thekingofcity <3353040+thekingofcity@users.noreply.github.com>
2022-10-28 21:33:09 +0800

    #99 Lazy load components

--- src/components/app-root.tsx
+++ src/components/app-root.tsx
@@ -1,24 +1,33 @@
-import React from 'react';
-import WindowHeader from './page-header/window-header';
-import SvgWrapper from './svg-wrapper';
-import ToolsPanel from './panel/tools/tools';
-import DetailsPanel from './panel/details/details';
+import React, { Suspense } from 'react';
 import { RmgErrorBoundary, RmgWindow } from '@railmapgen/rmg-components';
 import { Flex } from '@chakra-ui/react';
+
+const WindowHeader = React.lazy(() => import(/* webpackChunkName: "WindowHeader" */ './page-header/window-header'));
+const ToolsPanel = React.lazy(() => import(/* webpackChunkName: "ToolsPanel" */ './panel/tools/tools'));
+const SvgWrapper = React.lazy(() => import(/* webpackChunkName: "SvgWrapper" */ './svg-wrapper'));
+const DetailsPanel = React.lazy(() => import(/* webpackChunkName: "DetailsPanel" */ './panel/details/details'));
 
 export default function AppRoot() {
     return (
         <RmgWindow>
-            <WindowHeader />
-            <RmgErrorBoundary allowReset>
-                <Flex direction="row" height="100%" overflow="hidden" sx={{ position: 'relative' }}>
-                    {/* `position: 'relative'` is used to make sure RmgSidePanel in DetailsPanel
-                        have the right parent container for its `position: 'absolute'` calculation. */}
-                    <ToolsPanel />
-                    <SvgWrapper />
-                    <DetailsPanel />
-                </Flex>
-            </RmgErrorBoundary>
+            <Suspense
+                fallback={
+                    <p style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}>
+                        RMP protocol checked
+                    </p>
+                }
+            >
+                <WindowHeader />
+                <RmgErrorBoundary allowReset>
+                    <Flex direction="row" height="100%" overflow="hidden" sx={{ position: 'relative' }}>
+                        {/* `position: 'relative'` is used to make sure RmgSidePanel in DetailsPanel
+                            have the right parent container for its `position: 'absolute'` calculation. */}
+                        <ToolsPanel />
+                        <SvgWrapper />
+                        <DetailsPanel />
+                    </Flex>
+                </RmgErrorBoundary>
+            </Suspense>
         </RmgWindow>
     );
 }
