c145b99e285ed782eb66ab19eeac460c6dc9ee43
Chito Wong <chi.to.wong@outlook.com>
2023-02-05 14:00:42 +0800

    #174 Use upload-artefacts action in build workflow

--- scripts/build.sh
+++ scripts/build.sh
@@ -16,7 +16,6 @@ git config --global user.email 'github-actions[bot]@users.noreply.github.com'
 # variables
 export APP_NAME=$(node -p "require('./package.json').name")
 BRANCH=$(git branch | grep \* | cut -d ' ' -f2 | tr '/' '.')
-UAT_REPO_NAME=rmg-repositories
 
 # npm config
 npm config set tag-version-prefix "${APP_NAME}-"
@@ -35,12 +34,8 @@ else
   # git tag -a "${APP_NAME}-${RMG_VER}" -m "${APP_NAME}-${RMG_VER}"
 fi
 
-
 ### BUILD
-mkdir -p $UAT_REPO_NAME/"$APP_NAME"/
-
 CI='' npm run build
-cp -r build/ $UAT_REPO_NAME/"$APP_NAME"/"$RMG_VER"/
 
 ### PUSH TAG AND COMMIT
 if [ "$BRANCH" = "main" ]
@@ -48,12 +43,5 @@ then
   git push --atomic origin HEAD "${APP_NAME}-${RMG_VER}"
 fi
 
-### UPLOAD ARTIFACTS
-cd $UAT_REPO_NAME/
-git add .
-git commit -m "Build version $APP_NAME-$RMG_VER"
-git push --force
-
-
 echo "Build Success: $APP_NAME-$RMG_VER"
-echo "::set-output name=RMG_VER::$RMG_VER"
+echo "RMG_VER=$RMG_VER" >> $GITHUB_OUTPUT
