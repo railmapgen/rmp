16d88e43387b7fee03d4410e3a4b4d3d0045e3f8
thekingofcity <3353040+thekingofcity@users.noreply.github.com>
2023-04-01 14:02:22 +0800

    #226 MTR race days line style

---
+++ src/components/svgs/lines/styles/mtr-race-day.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { CityCode, MonoColour } from '@railmapgen/rmg-palette-resources';
+import { LinePathAttributes, LinePathType, LineStyle, LineStyleComponentProps } from '../../../../constants/lines';
+import { ColorField, AttributesWithColor } from '../../../panels/details/color-field';
+import { LineStyleType } from '../../../../constants/lines';
+
+const MTRRaceDays = (props: LineStyleComponentProps<MTRRaceDaysAttributes>) => {
+    const { id, path, styleAttrs, newLine, handleClick } = props;
+    const { color = defaultMTRRaceDaysAttributes.color } = styleAttrs ?? defaultMTRRaceDaysAttributes;
+
+    const onClick = React.useCallback(
+        (e: React.MouseEvent<SVGPathElement, MouseEvent>) => handleClick(id, e),
+        [id, handleClick]
+    );
+
+    return (
+        <path
+            id={id}
+            d={path}
+            fill="none"
+            stroke={color[2]}
+            strokeWidth="5"
+            strokeLinecap="butt"
+            strokeDasharray="5 2.5"
+            cursor="pointer"
+            onClick={onClick}
+        />
+    );
+};
+
+/**
+ * MTRRaceDays specific props.
+ */
+export interface MTRRaceDaysAttributes extends LinePathAttributes, AttributesWithColor {}
+
+const defaultMTRRaceDaysAttributes: MTRRaceDaysAttributes = {
+    color: [CityCode.Hongkong, 'twl', '#E2231A', MonoColour.white],
+};
+
+const mtrRaceDaysFields = [
+    {
+        type: 'custom',
+        component: <ColorField type={LineStyleType.MTRRaceDays} defaultAttrs={defaultMTRRaceDaysAttributes} />,
+    },
+];
+
+const mtrRaceDays: LineStyle<MTRRaceDaysAttributes> = {
+    component: MTRRaceDays,
+    defaultAttrs: defaultMTRRaceDaysAttributes,
+    // TODO: fix this
+    // @ts-ignore-error
+    fields: mtrRaceDaysFields,
+    metadata: {
+        displayName: 'panel.details.line.mtrRaceDays.displayName',
+        supportLinePathType: [LinePathType.Diagonal, LinePathType.Perpendicular, LinePathType.Simple],
+    },
+};
+
+export default mtrRaceDays;
