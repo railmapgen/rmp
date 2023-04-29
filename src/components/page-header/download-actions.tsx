b2e99681afb4d191ff386671216173a3fb5df756
thekingofcity <3353040+thekingofcity@users.noreply.github.com>
2023-04-29 14:09:04 +0800

    #181 Add id to download menu

--- src/components/page-header/download-actions.tsx
+++ src/components/page-header/download-actions.tsx
@@ -182,7 +182,7 @@ export default function DownloadActions() {
     };
 
     return (
-        <Menu>
+        <Menu id="download">
             <MenuButton as={IconButton} size="sm" variant="ghost" icon={<MdDownload />} />
             <MenuList>
                 <MenuItem icon={<MdSave />} onClick={handleDownloadJson}>
