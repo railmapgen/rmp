7ae39ca534775bf56d778080bff215be00a83e12
thekingofcity <3353040+thekingofcity@users.noreply.github.com>
2023-12-03 18:06:36 +0800

    #529 Polyfill `paint-order` for SVG 1.1

--- src/util/download.ts
+++ src/util/download.ts
@@ -66,6 +66,12 @@ export const makeImages = async (
         if (e === null) return; // no element in the canvas uses this class
         const style = window.getComputedStyle(e);
         elem.querySelectorAll(className).forEach(el => {
+            // Polyfill paint-order used in .rmp-name-outline for Adobe Illustrator.
+            // This is an SVG 2 specification and SVG 2 is not finalized or released yet.
+            // https://www.w3.org/TR/SVG2/painting.html#PaintOrder
+            if (className === '.rmp-name-outline')
+                el.insertAdjacentElement('afterend', el.cloneNode(true) as SVGElement);
+
             styleSet.forEach(styleName => {
                 el.setAttribute(styleName, style.getPropertyValue(styleName));
             });
