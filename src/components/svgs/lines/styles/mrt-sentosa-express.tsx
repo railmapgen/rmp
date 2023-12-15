e1ddbac3d7278ffe6dc2296089b62ebca20bf61e
thekingofcity <3353040+thekingofcity@users.noreply.github.com>
2023-12-15 15:14:53 +0000

    #545 Singapore MRT under construction and Sentosa Express line style

---
+++ src/components/svgs/lines/styles/mrt-sentosa-express.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { AttrsProps } from '../../../../constants/constants';
+import { LinePathAttributes, LinePathType, LineStyle, LineStyleComponentProps } from '../../../../constants/lines';
+
+const MRTSentosaExpress = (props: LineStyleComponentProps<MRTSentosaExpressAttributes>) => {
+    const { id, path, handleClick } = props;
+
+    const onClick = React.useCallback(
+        (e: React.MouseEvent<SVGPathElement, MouseEvent>) => handleClick(id, e),
+        [id, handleClick]
+    );
+
+    return (
+        <g id={id}>
+            <path
+                d={path}
+                fill="none"
+                stroke="black"
+                strokeWidth="5"
+                strokeDasharray="0 10 10 10"
+                strokeLinecap="round"
+                cursor="pointer"
+                onClick={onClick}
+            />
+        </g>
+    );
+};
+
+/**
+ * MRTSentosaExpress has no specific attributes.
+ */
+export interface MRTSentosaExpressAttributes extends LinePathAttributes {}
+
+const defaultMRTSentosaExpressAttributes: MRTSentosaExpressAttributes = {};
+
+const attrsComponent = (_: AttrsProps<MRTSentosaExpressAttributes>) => {
+    return null;
+};
+
+const mrtSentosaExpress: LineStyle<MRTSentosaExpressAttributes> = {
+    component: MRTSentosaExpress,
+    defaultAttrs: defaultMRTSentosaExpressAttributes,
+    attrsComponent,
+    metadata: {
+        displayName: 'panel.details.lines.mrtSentosaExpress.displayName',
+        supportLinePathType: [LinePathType.Diagonal, LinePathType.Perpendicular, LinePathType.RotatePerpendicular],
+    },
+};
+
+export default mrtSentosaExpress;
---
+++ src/components/svgs/lines/styles/mrt-under-construction.tsx
@@ -0,0 +1,79 @@
+import { RmgFields, RmgFieldsField } from '@railmapgen/rmg-components';
+import { CityCode, MonoColour } from '@railmapgen/rmg-palette-resources';
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import { AttrsProps } from '../../../../constants/constants';
+import {
+    LinePathAttributes,
+    LinePathType,
+    LineStyle,
+    LineStyleComponentProps,
+    LineStyleType,
+} from '../../../../constants/lines';
+import { AttributesWithColor, ColorField } from '../../../panels/details/color-field';
+
+const MRTUnderConstruction = (props: LineStyleComponentProps<MRTUnderConstructionAttributes>) => {
+    const { id, path, styleAttrs, handleClick } = props;
+    const { color = defaultMRTUnderConstructionAttributes.color } = styleAttrs ?? defaultMRTUnderConstructionAttributes;
+
+    const onClick = React.useCallback(
+        (e: React.MouseEvent<SVGPathElement, MouseEvent>) => handleClick(id, e),
+        [id, handleClick]
+    );
+
+    return (
+        <g id={id}>
+            <path d={path} fill="none" stroke={color[2]} strokeWidth="5" strokeDasharray="0 10" strokeLinecap="round" />
+            <path
+                d={path}
+                fill="none"
+                stroke="white"
+                strokeOpacity="0"
+                strokeWidth={5}
+                cursor="pointer"
+                onClick={onClick}
+            />
+        </g>
+    );
+};
+
+/**
+ * MRTUnderConstruction specific props.
+ */
+export interface MRTUnderConstructionAttributes extends LinePathAttributes, AttributesWithColor {}
+
+const defaultMRTUnderConstructionAttributes: MRTUnderConstructionAttributes = {
+    color: [CityCode.Beijing, 'bj1', '#c23a30', MonoColour.white],
+};
+
+const attrsComponent = (props: AttrsProps<MRTUnderConstructionAttributes>) => {
+    const { id, attrs, handleAttrsUpdate } = props;
+    const { t } = useTranslation();
+
+    const fields: RmgFieldsField[] = [
+        {
+            type: 'custom',
+            label: 'color',
+            component: (
+                <ColorField
+                    type={LineStyleType.MRTUnderConstruction}
+                    defaultTheme={defaultMRTUnderConstructionAttributes.color}
+                />
+            ),
+        },
+    ];
+
+    return <RmgFields fields={fields} />;
+};
+
+const mrtUnderConstruction: LineStyle<MRTUnderConstructionAttributes> = {
+    component: MRTUnderConstruction,
+    defaultAttrs: defaultMRTUnderConstructionAttributes,
+    attrsComponent,
+    metadata: {
+        displayName: 'panel.details.lines.mrtUnderConstruction.displayName',
+        supportLinePathType: [LinePathType.Diagonal, LinePathType.Perpendicular, LinePathType.RotatePerpendicular],
+    },
+};
+
+export default mrtUnderConstruction;
