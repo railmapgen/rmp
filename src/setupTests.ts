da9bc689d312837d07398c41108f7cbe6bd20bec
Chito Wong <chi.to.wong@outlook.com>
2024-02-03 17:01:33 +0800

    #623 Remove redux-mock-store as it is EOL

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,14 +1,15 @@
-import infoJson from '../info.json';
+import { vi } from 'vitest';
 
 const originalFetch = global.fetch;
-global.fetch = (...args) => {
+global.fetch = vi.fn().mockImplementation((...args: any[]) => {
     if (args[0].toString().includes('/info.json')) {
         return Promise.resolve({
             ok: true,
             status: 200,
-            json: () => Promise.resolve(infoJson),
+            json: () => import('../info.json').then(module => module.default),
         }) as any;
     } else {
-        return originalFetch(...args);
+        console.warn('No mocked response for', args[0]);
+        return originalFetch(args[0], args[1]);
     }
-};
+});
