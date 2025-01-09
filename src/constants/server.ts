4a8df24805e57b452adb7e831abe5ad772803f87
thekingofcity <3353040+thekingofcity@users.noreply.github.com>
2025-01-09 22:48:47 +0800

    fix https in server constants

--- src/constants/server.ts
+++ src/constants/server.ts
@@ -1,2 +1,2 @@
-export const subscription_endpoint = 'http://railmapgen.org/v1/subscription';
-export const random_station_names_endpoint = 'http://railmapgen.org/v1/randomStationNames';
+export const subscription_endpoint = 'https://railmapgen.org/v1/subscription';
+export const random_station_names_endpoint = 'https://railmapgen.org/v1/randomStationNames';
