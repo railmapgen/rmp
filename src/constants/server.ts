769b6eba8d55690e278295153b6164d83668f1d8
thekingofcity <3353040+thekingofcity@users.noreply.github.com>
2025-04-20 15:53:31 -0400

    #1027 `Refered` in Tauri fetch

--- src/constants/server.ts
+++ src/constants/server.ts
@@ -6,5 +6,16 @@ export const random_station_names_endpoint = 'https://railmapgen.org/v1/randomSt
 export const isTauri = rmgRuntime.getInstance() === 'Tauri' && '__TAURI__' in window.parent;
 
 // set fetch to tauri fetch if in tauri
-// @ts-expect-error global TAURI is injected in rmg-home
-export const fetch = isTauri ? (window.parent.__TAURI__.http.fetch as typeof window.fetch) : window.fetch;
+export const fetch = isTauri
+    ? async (url: RequestInfo | URL, init?: RequestInit) => {
+          const headers = init?.headers || {};
+          // @ts-expect-error global TAURI is injected in rmg-home
+          return window.parent.__TAURI__.http.fetch(url, {
+              ...init,
+              headers: {
+                  ...headers,
+                  referer: 'https://railmapgen.org/',
+              },
+          }) as ReturnType<typeof window.fetch>;
+      }
+    : window.fetch;
