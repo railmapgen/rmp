81dcd73e767417d688c81b9830d3f3cfdc42aaf4
thekingofcity <3353040+thekingofcity@users.noreply.github.com>
2026-03-05 21:17:28 +0800

    better type

--- src/components/svg-layer.tsx
+++ src/components/svg-layer.tsx
@@ -34,6 +34,7 @@ const SvgLayer = React.memo(
         );
         for (const element of elements) {
             if (element.type === 'line') {
+                const id = element.id as LineId;
                 const type = element.line!.attr.type;
                 const style = element.line!.attr.style;
                 const styleAttrs = element.line!.attr[style] as NonNullable<
@@ -43,9 +44,9 @@ const SvgLayer = React.memo(
                 const PreStyleComponent = lineStyles[style]?.preComponent as StyleComponent | undefined;
                 if (PreStyleComponent) {
                     layers[element.line!.attr.zIndex].pre.push(
-                        <g key={`${element.id}.pre`} id={`${element.id}.pre`}>
+                        <g key={`${id}.pre`} id={`${id}.pre`}>
                             <PreStyleComponent
-                                id={element.id as LineId}
+                                id={id}
                                 type={type}
                                 path={element.line!.path}
                                 styleAttrs={styleAttrs}
@@ -58,9 +59,9 @@ const SvgLayer = React.memo(
 
                 const StyleComponent = (lineStyles[style]?.component ?? UnknownLineStyle) as StyleComponent;
                 layers[element.line!.attr.zIndex].main.push(
-                    <g key={element.id as string} id={element.id as string}>
+                    <g key={id} id={id}>
                         <StyleComponent
-                            id={element.id as LineId}
+                            id={id}
                             type={type}
                             path={element.line!.path}
                             styleAttrs={styleAttrs}
@@ -73,9 +74,9 @@ const SvgLayer = React.memo(
                 const PostStyleComponent = lineStyles[style]?.postComponent as StyleComponent | undefined;
                 if (PostStyleComponent) {
                     layers[element.line!.attr.zIndex].post.push(
-                        <g key={`${element.id}.post`} id={`${element.id}.post`}>
+                        <g key={`${id}.post`} id={`${id}.post`}>
                             <PostStyleComponent
-                                id={element.id as LineId}
+                                id={id}
                                 type={type}
                                 path={element.line!.path}
                                 styleAttrs={styleAttrs}
@@ -86,6 +87,7 @@ const SvgLayer = React.memo(
                     );
                 }
             } else if (element.type === 'station') {
+                const id = element.id as StnId;
                 const attr = element.station!;
                 const type = attr.type as StationType;
 
@@ -98,7 +100,7 @@ const SvgLayer = React.memo(
                             transform={`translate(${attr.x}, ${attr.y})`}
                         >
                             <PreStationComponent
-                                id={element.id as StnId}
+                                id={id}
                                 x={attr.x}
                                 y={attr.y}
                                 attrs={attr}
@@ -112,9 +114,9 @@ const SvgLayer = React.memo(
 
                 const StationComponent = allStations[type]?.component ?? UnknownNode;
                 layers[element.station!.zIndex].main.push(
-                    <g key={element.id} id={element.id as string} transform={`translate(${attr.x}, ${attr.y})`}>
+                    <g key={id} id={id} transform={`translate(${attr.x}, ${attr.y})`}>
                         <StationComponent
-                            id={element.id as StnId}
+                            id={id}
                             x={attr.x}
                             y={attr.y}
                             attrs={attr}
@@ -128,13 +130,9 @@ const SvgLayer = React.memo(
                 const PostStationComponent = allStations[type]?.postComponent;
                 if (PostStationComponent) {
                     layers[element.station!.zIndex].post.push(
-                        <g
-                            key={`${element.id}.post`}
-                            id={`${element.id}.post`}
-                            transform={`translate(${attr.x}, ${attr.y})`}
-                        >
+                        <g key={`${id}.post`} id={`${id}.post`} transform={`translate(${attr.x}, ${attr.y})`}>
                             <PostStationComponent
-                                id={element.id as StnId}
+                                id={id}
                                 x={attr.x}
                                 y={attr.y}
                                 attrs={attr}
@@ -146,6 +144,7 @@ const SvgLayer = React.memo(
                     );
                 }
             } else if (element.type === 'misc-node') {
+                const id = element.id as MiscNodeId;
                 const attr = element.miscNode!;
                 const type = attr.type as MiscNodeType;
 
@@ -158,7 +157,7 @@ const SvgLayer = React.memo(
                             transform={`translate(${attr.x}, ${attr.y})`}
                         >
                             <PreMiscNodeComponent
-                                id={element.id as MiscNodeId}
+                                id={id}
                                 x={attr.x}
                                 y={attr.y}
                                 // @ts-expect-error
@@ -173,9 +172,9 @@ const SvgLayer = React.memo(
 
                 const MiscNodeComponent = miscNodes[type]?.component ?? UnknownNode;
                 layers[element.miscNode!.zIndex].main.push(
-                    <g key={element.id} id={element.id as string} transform={`translate(${attr.x}, ${attr.y})`}>
+                    <g key={id} id={id} transform={`translate(${attr.x}, ${attr.y})`}>
                         <MiscNodeComponent
-                            id={element.id as MiscNodeId}
+                            id={id}
                             x={attr.x}
                             y={attr.y}
                             // @ts-expect-error
@@ -190,13 +189,9 @@ const SvgLayer = React.memo(
                 const PostMiscNodeComponent = miscNodes[type]?.postComponent;
                 if (PostMiscNodeComponent) {
                     layers[element.miscNode!.zIndex].post.push(
-                        <g
-                            key={`${element.id}.post`}
-                            id={`${element.id}.post`}
-                            transform={`translate(${attr.x}, ${attr.y})`}
-                        >
+                        <g key={`${id}.post`} id={`${id}.post`} transform={`translate(${attr.x}, ${attr.y})`}>
                             <PostMiscNodeComponent
-                                id={element.id as MiscNodeId}
+                                id={id}
                                 x={attr.x}
                                 y={attr.y}
                                 // @ts-expect-error
